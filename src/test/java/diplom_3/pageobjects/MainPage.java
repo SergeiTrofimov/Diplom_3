f984a0ad365f2de6ba5bfbaa51c0f2268dba1a46
Sergei Trofimov <divisor.null@yandex.com>
2022-11-10 19:55:33 +0300

    1.Первичная инфраструктура

--- src/test/java/diplom_3/pageobjects/MainPage.java
+++ src/test/java/diplom_3/pageobjects/MainPage.java
@@ -1,4 +1,22 @@
 package diplom_3.pageobjects;
 
-public class MainPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//Локаторы
+public class MainPage extends MainAbstractPage{
+    private By personalAccount = By.name("Личный Кабинет"); // Локатор кнопки "Личный кабинет"
+
+    public MainPage(WebDriver driver) { // конструктор главной страницы
+        super(driver);
+    }
+
+    //Методы
+    public void clickPersonalAccount() { // кликаем кнопку "Личный кабинет"
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfElementLocated(personalAccount));
+        driver.findElement(personalAccount).click();
+    }
 }
