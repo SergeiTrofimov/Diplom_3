306e54038c684cc312707fa3807cd4dab563104c
Sergei Trofimov <divisor.null@yandex.com>
2022-11-10 20:03:14 +0300

    1.Первичная инфраструктура

--- src/test/java/diplom_3/BasicUITestChrome.java
+++ src/test/java/diplom_3/BasicUITestChrome.java
@@ -1,5 +1,6 @@
 package diplom_3;
 
+import diplom_3.pageobjects.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -15,8 +16,7 @@ public class BasicUITestChrome {
         WebDriverManager.chromedriver().setup(); // инициализируем драйвер под хром
         webDriver = new ChromeDriver(); // создаем объект драйвера под хром
         webDriver.get(Constants.hostname); // открываем страницу
-       // ScooterLandingPage objScooterLandingPage = new ScooterLandingPage(webDriver);
-       // objScooterLandingPage.setCookieRemoveButton();
+        MainPage objMainPage = new MainPage(webDriver);
     }
 
     @AfterClass
--- src/test/java/diplom_3/RegistrationTestChrome.java
+++ src/test/java/diplom_3/RegistrationTestChrome.java
@@ -1,6 +1,19 @@
 package diplom_3;
 
-public class RegistrationTestChrome {
+import diplom_3.pageobjects.MainPage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RegistrationTestChrome extends BasicUITestChrome {
+   MainPage mainPage = new MainPage(webDriver);
+
     //Успешную регистрацию.
+    @Test
+    public void canOrderScooter() {
+
+
+
+    }
     //Ошибку для некорректного пароля. Минимальный пароль — шесть символов.
 }
