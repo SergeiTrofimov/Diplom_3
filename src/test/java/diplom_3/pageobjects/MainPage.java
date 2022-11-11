7ede8fa0a7a673d7aa6babc3c46fd17254c493d0
Sergei Trofimov <divisor.null@yandex.com>
2022-11-11 14:50:50 +0300

    1.Добавлены тесты для регистрации 2.Добавлены тесты для логина 3.Добавлены тесты для личного кабинета

--- src/test/java/diplom_3/pageobjects/MainPage.java
+++ src/test/java/diplom_3/pageobjects/MainPage.java
@@ -10,6 +10,10 @@ public class MainPage extends MainAbstractPage {
     private By personalAccountTopButton = By.xpath("//*[@id=\"root\"]/div/header/nav/a/p"); // Локатор кнопки "Личный кабинет"
     private By personalAccountEnterButton = By.xpath("//*[@id=\"root\"]/div/main/section[2]/div/button");
     private By burgerText = By.xpath("//*[@id=\"root\"]/div/main/section[1]/h1");
+    private By bunConstructor = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[1]/span");
+    private By sauceConstructor = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[2]/span");
+    private By mainConstructor = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[3]/span");
+
 
     public MainPage(WebDriver driver) { // конструктор главной страницы
         super(driver);
@@ -22,10 +26,18 @@ public class MainPage extends MainAbstractPage {
         driver.findElement(personalAccountTopButton).click();
     }
 
+    public void clickBun() {
+        driver.findElement(bunConstructor).click();
+    }
+    public void clickSauce() {
+        driver.findElement(sauceConstructor).click();
+    }
+    public void clickMain() {
+        driver.findElement(mainConstructor).click();
+    }
     public void clickPersonalAccountEnterButton() {
         driver.findElement(personalAccountEnterButton).click();
     }
-
     public boolean isBurgerTextIsDisplayed() {
         new WebDriverWait(driver, 15)
                 .until(ExpectedConditions.visibilityOfElementLocated(burgerText));
